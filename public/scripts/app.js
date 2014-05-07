47abf095616c476ddbd20dec16387c81a29c44f8
Chetan Anand <chetananand.a@gmail.com>
2014-05-07 05:26:33 -0700

    UI Changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,16 @@
-var app = angular.module('studentApp', [])
+var app = angular.module('studentApp', ['ui.bootstrap', 'ui.router']);
 
 app.controller('appController', function($scope) {
 	console.log("Hello");
 });
+
+/* Sticky Header */
+var $menu = document.querySelector('#nav-menu');
+window.onscroll = function() {
+	console.log("Scroll " + window.pageYOffset)
+	if(window.pageYOffset > 60){
+		$menu.className = "menu cf menu-sticky";
+	}else{
+		$menu.className = "menu cf";
+	}
+}
