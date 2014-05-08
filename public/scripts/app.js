1d93d7acd720ea1e35705c96378467ed057b8c58
Chetan Anand <chetananand.a@gmail.com>
2014-05-08 10:38:28 -0700

    UI Changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,74 @@
 var app = angular.module('studentApp', ['ui.bootstrap', 'ui.router']);
 
-app.controller('appController', function($scope) {
-	console.log("Hello");
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+	$urlRouterProvider.otherwise('/home');
+	$stateProvider
+		.state('home', {
+			url : '/home',
+			templateUrl : '/html/home.html'
+		})
+		.state('events', {
+			url : '/events',
+			templateUrl : '/html/events.html'			
+		})
+		.state('about', {
+			url : '/about'
+		})		
+
+		// use the HTML5 History API
+		$locationProvider.html5Mode(true);
 });
+
+app.run(function($rootScope) {
+	$rootScope
+        .$on('$stateChangeStart', 
+            function(event, toState, toParams, fromState, fromParams){ 
+                console.log("State Change: transition begins!");
+        });
+
+    $rootScope
+        .$on('$stateChangeSuccess',
+            function(event, toState, toParams, fromState, fromParams){ 
+                console.log("State Change: State change success!");
+        });
+
+    $rootScope
+        .$on('$stateChangeError',
+            function(event, toState, toParams, fromState, fromParams){ 
+                console.log("State Change: Error!");
+        });
+
+    $rootScope
+        .$on('$stateNotFound',
+            function(event, toState, toParams, fromState, fromParams){ 
+                console.log("State Change: State not found!");
+        });
+
+    $rootScope
+        .$on('$viewContentLoaded',
+            function(event, viewConfig){ 
+                console.log("View Load: the view is loaded, and DOM rendered!");
+        });
+});
+
+app.controller('appController', function($scope) {
+	
+});
+
+app.controller('studentListController', function($scope) {
+	
+});
+
+app.controller('eventListController', function($scope) {
+
+})
+	
+
 
 /* Sticky Header */
 var $menu = document.querySelector('#nav-menu');
 window.onscroll = function() {
-	console.log("Scroll " + window.pageYOffset)
 	if(window.pageYOffset > 60){
 		$menu.className = "menu cf menu-sticky";
 	}else{
--- server.js
+++ server.js
@@ -10,5 +10,13 @@ var configDB = require(__dirname + '/public/scripts/database.js');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
 
+
+//http://vladimirfeskov.com/posts/angularjs-html5-mode-setup-use-and-seo
+
+app.get('/[^\.]+$', function(req, res){
+    res.set('Content-Type', 'text/html')
+        .sendfile(__dirname + '/public/html/index.html');
+});
+
 app.listen(port);
 console.log("Port 8080 is listening to Student api service requests");
